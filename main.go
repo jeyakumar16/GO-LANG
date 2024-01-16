8deaf58a7185a4723413538a538d9d65115116be
jeyakumar16 <112744989+jeyakumar16@users.noreply.github.com>
2024-01-16 11:01:34 +0530

    Update main.go
    Edited main.go with a new code for comparing the elapsed time between different allocation techniques for slices

--- main.go
+++ main.go
@@ -1,55 +1,30 @@
+//Simple Go Program to compare the compilation time between preallocation of capacity of slices and slices without preallocation
+//using the time package
+
+//time.Now() return the current monotonic clock readings
+//time.Duration represents the time elapsed in nanoseconds
+//time.Since(t) is equivalent to time.Now().Sub(t) --- provides the elapsed time since t
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	intArr := [...]int32{1, 2, 3}
-	fmt.Println(intArr)
+	var n int = 100000
+	var testSlice []int = []int{}
+	var testSlice2 = make([]int, 0, n)
+	fmt.Printf("Total time taken without preallocation: %v\n", timeLoop(testSlice, n))
+	fmt.Printf("Total time taken with preallocation: %v\n", timeLoop(testSlice2, n))
 
-	var intSlice []int32 = []int32{4, 5, 6}
-	fmt.Printf("The length is %v with the capacity %v", len(intSlice), cap(intSlice))
-	intSlice = append(intSlice, 7)
-	fmt.Printf("\nThe length is %v with the capacity %v\n", len(intSlice), cap(intSlice))
+}
 
-	var intSlice2 []int32 = []int32{8, 9}
-	intSlice = append(intSlice, intSlice2...)
-
-	fmt.Println(intSlice)
-
-	var intSlice3 []int32 = make([]int32, 3, 8)
-	fmt.Printf("\nThe length is %v with the capacity %v\n", len(intSlice3), cap(intSlice3))
-
-	var myMap map[string]uint8 = make(map[string]uint8)
-	fmt.Println(myMap)
-
-	var myMap2 = map[string]uint8{"JK": 20, "Bharath": 21}
-	fmt.Println(myMap2["Bharath"])
-
-	var age, ok = myMap2["Hariesh"]
-	if ok {
-		fmt.Printf("The age is %v\n", age)
-	} else {
-		fmt.Println("Invalid Name, key doesn't exist")
+func timeLoop(slice []int, n int) time.Duration {
+	var t0 = time.Now()
+	for len(slice) < n {
+		slice = append(slice, 1)
 	}
-
-	for name, age := range myMap2 {
-		fmt.Printf("The name is %v and the their age is %v \n", name, age)
-	}
-
-	for i, v := range intArr {
-		fmt.Printf("The index is %v and the corresponding value is %v \n", i, v)
-	}
-
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-	}
-
-	var i int = 0
-	for {
-		if i > 10 {
-			break
-		}
-		fmt.Println(i)
-		i++
-	}
+	return time.Since(t0)
 }
